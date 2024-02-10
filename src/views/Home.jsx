e42a1f0afafcc2ac85c7327ee245300c649895e4
Moustafa Nady <mostafanadii93@gmail.com>
2024-02-10 19:09:11 +0100

    updates

---
+++ src/views/Home.jsx
@@ -0,0 +1,14 @@
+import SearchBar from "../components/SearchBar";
+
+const Home = () => {
+  return (
+    <>
+      <div className="home">
+        <SearchBar />
+        
+      </div>
+    </>
+  );
+};
+
+export default Home;
